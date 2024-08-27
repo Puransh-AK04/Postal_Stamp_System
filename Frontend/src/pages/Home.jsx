9e0aec01bb0d34d76d346f7c2a5358f93ef15855
kyash99252 <yashkadam99252@gmail.com>
2024-08-27 12:44:39 +0530

    Basic Navbar & Login

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="mt-5 mx-20">
+      <div className=" bg-navbar-blue h-10 align-center p-2 pl-4">
+        <span className="font-serif font-thin text-amber-300">Home</span>
+      </div>
+      <div className="mt-5 mx-10 border-black border-2 shadow">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum repudiandae cupiditate voluptate dolor, eaque asperiores odit reprehenderit maiores aliquam repellat consectetur impedit? Earum maxime omnis quod, doloribus facere saepe a?</div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ Frontend/src/routes.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.jsx";
+import LoginLayout from "./layouts/LoginLayout.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Login from "./pages/Login.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "about", element: <About /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginLayout />,
+    children: [
+        { path: "/login", element: <Login/> },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
